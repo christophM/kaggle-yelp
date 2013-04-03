b6d5c358449b4378ede42618d49b8d2ca8930837
Christoph Molnar <christoph.molnar@gmail.com>
2013-04-03 16:00:30 +0200

    changing to pandas.merge instead of sql

--- src/python/features.py
+++ src/python/features.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import pandas.io.sql as pd_sql
 import math
 from datetime import datetime
-import sqlite3 as db
-import os
 
 
 data_path = "./data/"
@@ -34,6 +31,8 @@ users_test = pd.read_csv(test_path + "yelp_test_set_user.csv", header = 0)
 # businesses_test =
 
 def processBusinessess(business, checkin):
+    business = business.drop_duplicates()
+    checkin = checkin.drop_duplicates()
     ## delete unused columns
     delete_cols = ["categories", "full_address", "neighborhoods", "name", "state", "type"]
     business = business.drop(delete_cols, axis = 1)
@@ -43,15 +42,16 @@ def processBusinessess(business, checkin):
     business.city = business.city.map(lambda x: x if x in frequent_cities else "Other")
     ## aggregate checkins
     # drop type variable
-    #checkin = checkin.drop("type", axis = 1)
+    checkin = checkin.drop("type", axis = 1)
     # replace NaN with 0 as reported in forum
-    #checkin = checkin.fillna(0)
-    #checkin['nCheckins'] = checkin.apply(lambda row: sum(row[2:]), axis = 1)
+    checkin = checkin.fillna(0)
+    checkin['nCheckins'] = checkin.apply(lambda row: sum(row[2:]), axis = 1)
     ## drop checkin info
-    #checkin = checkin[['business_id', 'nCheckins']]
+    checkin = checkin[['business_id', 'nCheckins']]
     ## merge business and checkin data
-    #return (pd.merge(business, checkin, how = "left", on = "business_id"))
-    return(business)
+    business = pd.merge(business, checkin, how = "left", on = "business_id")
+    business["nCheckins"] = business.nCheckins.fillna(0)
+    return (business)
 
 
 
@@ -62,7 +62,7 @@ def processReviews(reviews):
     reviews['year'] = reviews.date.map(lambda x: x.year)
     reviews['weekday'] = reviews.date.map(lambda x: x.weekday())
     ## drop the text
-    reviews = reviews.drop("text", axis = 1)
+    reviews = reviews.drop(["text", "type"], axis = 1)
     return(reviews)
 
 def processUsers(users):
@@ -88,17 +88,14 @@ userFeatTest = processUsers(users_test)
 userFeatTrain = processUsers(users_train)
 
 
-print("creating databases")
-os.remove("./data/yelp.db")
-con = db.connect("./data/yelp.db")
-pd_sql.write_frame(busyFeatTest, "businessTest", con)
-pd_sql.write_frame(busyFeatTrain, "businessTrain", con)
-pd_sql.write_frame(userFeatTrain, "userTrain", con)
-pd_sql.write_frame(userFeatTest, "userTest", con)
-pd_sql.write_frame(revFeatTest, "reviewTest", con)
-pd_sql.write_frame(revFeatTrain, "reviewTrain", con)
 print("merging")
-os.system("sqlite3 -batch -init ./src/sql/mergeAll.sql ./data/yelp.db")
+print("   test")
 
-print("finished")
+revBusTest = pd.merge(revFeatTest, busyFeatTest, on = "business_id", how = "left", suffixes = ("_rev", "_biz"))
+featuresTest = pd.merge(revBusTest, userFeatTest, on = "user_id", how = "left", suffixes = ("_rev", "_user"))
+featuresTest.to_csv(test_path + "features-test.csv")
 
+print("   train")
+revBusTrain = pd.merge(revFeatTrain,  busyFeatTrain, on = "business_id", how = "left", suffixes = ("_rev", "_biz"))
+featuresTrain = pd.merge(revBusTrain, userFeatTrain, on = "user_id", how = "left", suffixes = ("_rev", "_user"))
+featuresTrain.to_csv(train_path + "features-train.csv")
