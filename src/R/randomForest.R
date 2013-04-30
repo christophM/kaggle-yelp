39387df400f9f9973ac25084637100f8ce7f0682
Christoph Molnar <christoph.molnar@gmail.com>
2013-04-30 21:46:50 +0200

    make rf.R ready just to look into randomForest on server

--- src/R/randomForest.R
+++ src/R/randomForest.R
@@ -3,36 +3,19 @@
 ## Try a simple random Forest
 ##
 ################################################################################
-load("./data/rdata/full-featured.RData")
+
 library("randomForest")
+train  <- read.csv("./data/train/features-train.csv")
+test <- read.csv("./data/test/features-test.csv")
+names(train)
 
 
-## impute missing values
-datImp <- apply(dat[)], 2, function(x) {
-  if(is.numeric(x)) {
-    m <- median(x)
-    x[is.na(x)] <- m
-  }
-  x
-})
-
-
-
-test <- datImp[which(is.na(datImp$votes_useful.rev)), ]
-train <- datImp[- which(is.na(datImp$votes_useful.rev)), ]
-
-names(dat)
-
-train <- na.omit(train)[1:10000, ]
-y = train$votes_useful.rev
-x = train[-which(names(train) %in% c("business_id", "user_id", "review_id", "votes_funny.rev", "votes_cool.rev", "votes_useful.rev"))]
-rf <- randomForest(y = log(y +1), x = x, do.trace = TRUE, ntree = 100)
+y = train$votes_useful
+x = train[-which(names(train) %in% c("votes_useful", "review_id", "date"))]
+rf <- randomForest(y = log(y +1), x = x, do.trace = TRUE, ntree = 500)
+print(x$mse)
 plot(rf)
 varImpPlot(rf)
-
-
-
-ggplot(train) + geom_point(aes(y = votes_useful.rev, x = date))
 
 
 predict(rf, test)
