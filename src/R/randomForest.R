65b108c8071f788dd913bee020729860e535579f
Christoph Molnar <christoph.molnar@gmail.com>
2013-04-30 23:41:43 +0200

    log of response

--- src/R/randomForest.R
+++ src/R/randomForest.R
@@ -10,12 +10,16 @@ test <- read.csv("./data/test/features-test.csv")
 names(train)
 
 
-y = train$votes_useful
+y = log(train$votes_useful + 1)
 x = train[-which(names(train) %in% c("votes_useful", "review_id", "date"))]
+set.seed(42)
 rf <- randomForest(y = log(y +1), x = x, do.trace = TRUE, ntree = 500)
+save(rf, file = "./models/randomForestR-1-5-2013.RData")
 print(x$mse)
 plot(rf)
 varImpPlot(rf)
 
 
 predict(rf, test)
+
+## prediction: dont forget to remove log
