b805d8e7d114705909b6d9110c6a99b308f25b50
Christoph Molnar <christoph.molnar@gmail.com>
2013-05-14 15:40:46 +0200

    more trees, more splits, gbm

--- src/python/train.py
+++ src/python/train.py
@@ -1,7 +1,8 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.externals import joblib
 import pandas as pd
 import numpy as np
+import pylab as pl
 import matplotlib.pyplot as plt
 from loss import rmsle
 
@@ -10,45 +11,63 @@ def readReviews(filename):
     features = data.drop(["votes_useful", "city", "date"], axis = 1).set_index("review_id")
     target = data.votes_useful.map(lambda x: np.log(x + 1))
     return target, features
-    
-def importances(model):
-    importances = model.feature_importances_
-    indices = np.argsort(importances)[::-1]
-    print("-------------------------------------------------------------------------")
-    print("Score on training data: " + str(model.score(features, target)))
-    print("Feature ranking:")
-    for f in xrange(importances.size - 1):
-        print "%d. feature %s (%f)" % (f + 1, features.columns[f], importances[indices[f]])
-     
+
+################################################################################
 print("reading data")
 target, features = readReviews("./data/train/features-train.csv")
 target_inTrain, features_inTrain = readReviews("./data/train/features-inTrain.csv")
 target_inTest, features_inTest = readReviews("./data/train/features-inTest.csv")
 
+################################################################################
 np.random.seed(42)
-model = RandomForestRegressor(compute_importances = True, oob_score = True, verbose = 2, n_jobs = 2, n_estimators = 45, max_features = "sqrt")
+params = {'n_estimators': 100, 'max_depth': 5, 'min_samples_split': 2, 'loss': 'ls', 'verbose': 2}
+model = GradientBoostingRegressor(**params)
 print("fitting model on " + str(len(features.columns)) + " features")
 model.fit(features_inTrain, target_inTrain)
 predicted = np.exp(model.predict(features_inTest)) - 1
 actual = target_inTest
 print("Rmsle score: " + str(rmsle(actual, predicted)))
-importances(model)
-## Print test set error
-## Input the RandomForestRegressor, test set feature and test set known values
-def rfErrCurve(rf_model,test_X,test_y):
-    p = []
-    for i,tree in enumerate(rf_model.estimators_):
-                p.insert(i,tree.predict(test_X))
-                print rmsle(np.mean(p,axis=0),test_y)
-print("error curve: ")
-print(rfErrCurve(model, features_inTest, actual))
+
+################################################################################
+
+# Plot training deviance
+
+# compute test set deviance
+test_score = np.zeros((params['n_estimators'],), dtype=np.float64)
+
+for i, y_pred in enumerate(model.staged_decision_function(features_inTest)):
+    test_score[i] = model.loss_(target_inTest, y_pred)
+
+pl.figure(figsize=(12, 6))
+pl.subplot(1, 2, 1)
+pl.title('Deviance')
+pl.plot(np.arange(params['n_estimators']) + 1, model.train_score_, 'b-',
+        label='Training Set Deviance')
+pl.plot(np.arange(params['n_estimators']) + 1, test_score, 'r-',
+        label='Test Set Deviance')
+pl.legend(loc='upper right')
+pl.xlabel('Boosting Iterations')
+pl.ylabel('Deviance')
+###############################################################################
+# Plot feature importance
+feature_importance = model.feature_importances_
+# make importances relative to max importance
+feature_importance = 100.0 * (feature_importance / feature_importance.max())
+sorted_idx = np.argsort(feature_importance)
+pos = np.arange(sorted_idx.shape[0]) + .5
+pl.subplot(1, 2, 2)
+pl.barh(pos, feature_importance[sorted_idx], align='center')
+pl.yticks(pos, features_inTest.columns[sorted_idx])
+pl.xlabel('Relative Importance')
+pl.title('Variable Importance')
+pl.show()
+################################################################################
 print("-------------------------------------------------------------------------")
 print("Refitting model with whole training data")
 model.fit(features, target)
 print("writing model to disk")
-# save random forest
-filename = "./models/rf_regressor.joblib.pkl"
+#save random forest
+filename = "./models/gbm_regressor.joblib.pkl"
 _ = joblib.dump(model, filename, compress = 0)
 
-importances(model)
 
